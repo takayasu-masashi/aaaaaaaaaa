89001ff35e3488cb4db19891bf7fc2eb3354601d
Yamashiro-yuta <65996680+Yamashiro-yuta@users.noreply.github.com>
2021-03-16 09:47:41 +0900

    Add files via upload

---
+++ Assets/IkinokoBattle/Scripts/RoundLight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundLight : MonoBehaviour
+{
+    // Start is called before the first frame update// Update is called once per frame
+    void Update()
+    {
+        //Y軸に対して、1秒間に-12度回転させる
+        transform.Rotate(new Vector3(0, -12) * Time.deltaTime);
+
+    }
+}
